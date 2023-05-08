15886b5f0302ff8b625867a49e48aa733898ba6b
Emilia-Victoria <emiliav.helsted@gmail.com>
2023-05-08 08:44:11 +0200

    Fix changeSuccess

--- ScrabbleBot/Scrabble.fs
+++ ScrabbleBot/Scrabble.fs
@@ -220,7 +220,7 @@ module Scrabble =
                 
                 aux st'
             | RCM (CMChangeSuccess newTiles) ->
-                let st' = mkState st.board st.dict st.playerNumber (updateHand MultiSet.empty List.Empty newTiles) st.playedTiles st.amountTiles
+                let st' = mkState st.board st.dict st.playerNumber (updateHand st.hand List.Empty newTiles) st.playedTiles st.amountTiles
                 aux st'
                 
             | RCM (CMPlayed (pid, ms, points)) ->
